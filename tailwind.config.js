ca982834a31036ed2a7b244e7af32fed2ae4a300
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-12-24 21:17:34 +0200

    feat: config `tailwindcss`

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/main/**/*.{js,ts,jsx,tsx}', './src/infrastructure/ui/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
