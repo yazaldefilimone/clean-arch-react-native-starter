032323dc2347c48d019d52e8a1a146c7d00d4899
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-12-24 22:11:16 +0200

    feat: add `statusBar` component

--- src/main/routes/index.tsx
+++ src/main/routes/index.tsx
@@ -1,4 +1,6 @@
 import { FunctionComponent } from 'react';
+import { StatusBar } from 'expo-status-bar';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from '~/infrastructure/ui/screens/home';
@@ -11,6 +13,7 @@ export const ScreenRoutes: FunctionComponent = () => {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 };
